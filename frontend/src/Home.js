d7af7021ae342e929e4c9cd87de42be83a4a368e
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-01-18 12:43:47 -0800

    Styling for background image

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="Home d-flex flex-column justify-content-center align-items-center">
-        <h1 className="font-weight-bold">Jobly</h1>
-        <p>All the jobs in one, convenient place.</p>
+      <div
+        id="HOME"
+        className="Home d-flex flex-column justify-content-center align-items-center"
+      >
+        <h1 className="border-text font-weight-bold">Jobly</h1>
+        <p className="border-text">All the jobs in one, convenient place.</p>
         {this.props.token !== null ? (
-          <h2>Welcome Back!</h2>
+          <h2 className="border-text">Welcome Back!</h2>
         ) : (
           <Link to="/login">
             <button className="btn btn-primary">Login</button>
